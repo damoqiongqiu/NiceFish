b84cfae411d135b5716599be6b5fceadb080866f
zhang.xiaofei <damoqiongqiu@126.com>
2017-01-20 21:15:06 +0800

    删掉不必要的内容；

--- src/app/manage/manage.routes.ts
+++ src/app/manage/manage.routes.ts
@@ -1,7 +1,6 @@
 import { ManageMainComponent } from './manage-main/manage-main.component';
 import { PostTableComponent } from './post-table/post-table.component';
 import { CommentTableComponent } from './comment-table/comment-table.component';
-import { TagTableComponent } from './tag-table/tag-table.component';
 import { UserTableComponent } from './user-table/user-table.component';
 import { SysParamComponent } from './sys-param/sys-param.component';
 
