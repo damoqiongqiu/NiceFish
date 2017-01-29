aea334353887c425ee8be158eaa81a1467158d2f
zhang.xiaofei <damoqiongqiu@126.com>
2017-01-29 15:00:29 +0800

    修改一个路径错误；

--- src/app/manage/post-table/post-table.component.ts
+++ src/app/manage/post-table/post-table.component.ts
@@ -12,7 +12,7 @@ import { PostTableService } from './services/post-table.service';
   ]
 })
 export class PostTableComponent implements OnInit {
-    @Input() dataURL:string="mock-data/post-mock.json";
+    @Input() dataURL:string="mock-data/postlist-mock.json";
 
 	  public postList:Array<any>;
     public maxSize:number = 5;
