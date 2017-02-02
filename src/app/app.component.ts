bc3528870befcf1d203f3590867a49e9711e9b3e
zhang.xiaofei <damoqiongqiu@126.com>
2017-02-02 13:47:22 +0800

    路由守卫案例；

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component,HostListener,ElementRef,Renderer} from '@angular/core';
 import { ActivatedRoute, Router, ActivatedRouteSnapshot, RouterState, RouterStateSnapshot } from '@angular/router';
 import { TranslateService } from 'ng2-translate';
-
-import 'rxjs/add/operator/merge';
-
 import { UserLoginService } from './user/user-login/user-login.service';
 import { UserRegisterService } from './user/user-register/user-register.service';
 import { User } from './user/model/user-model';
+import 'rxjs/add/operator/merge';
 
 @Component({
   selector: 'app-root',
--- src/app/manage/auth-guard.ts
+++ src/app/manage/auth-guard.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
+import { UserLoginService } from '../user/user-login/user-login.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router) {
+  	constructor(
+  		private router: Router,
+  		public userLoginService: UserLoginService) {
 
-  }
+  	}
   
-  canActivate(route: ActivatedRouteSnapshot,state: RouterStateSnapshot): boolean {
-    return true;
-  }
+  	canActivate(route: ActivatedRouteSnapshot,state: RouterStateSnapshot): boolean {
+  		//这里可以调用真实的服务进行验证
+  		// 	this.userLoginService.currentUser
+		     // .subscribe(
+			 // 	data => {
+					
+			 // 	},
+			 // 	error => console.error(error)
+			 // );
+    	return true;
+  	}
 }
