17f77bf6f5e67922eaff0b51aa9e692adf70f54a
zhang.xiaofei <damoqiongqiu@126.com>
2017-02-07 21:43:20 +0800

    跑个单元测试练练手；

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,8 +1,47 @@
-/* tslint:disable:no-unused-variable */
+import { async, inject, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { HomeComponent }   from './home.component';
+import { SocialChannelComponent } from './social-channel/social-channel.component';
+import { SitestatComponent } from '../sitestat/sitestat.component';
+import { OnlineContactComponent } from './online-contact/online-contact.component';
+import { SharedModule } from '../shared/shared.module';
 
-import { TestBed, async } from '@angular/core/testing';
-import { HomeComponent } from './home.component';
+import {homeRoutes} from './home.routes';
 
 describe('Component: Home', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        SharedModule
+      ],
+      declarations: [         
+        HomeComponent
+        // ,
+        // SocialChannelComponent,
+        // SitestatComponent,
+        // OnlineContactComponent 
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('true is true', () => {
+    expect(true).toBe(false);
+  });
 });
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
-import { RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { HomeComponent }   from './home.component';
 import { SocialChannelComponent } from './social-channel/social-channel.component';
 import { SitestatComponent } from '../sitestat/sitestat.component';
@@ -11,7 +11,7 @@ import {homeRoutes} from './home.routes';
 @NgModule({
     imports: [
         SharedModule,
-         RouterModule.forChild(homeRoutes)
+        RouterTestingModule.withRoutes(homeRoutes)
     ],
     exports: [],
     declarations: [
