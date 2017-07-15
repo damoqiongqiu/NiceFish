1d1e8edab42c3d88b1560f08e3070e27a82f78d4
zhang.xiaofei <damoqiongqiu@126.com>
2017-07-15 14:25:50 +0800

    rxjs的例子

--- src/app/post/postlist/postlist.component.ts
+++ src/app/post/postlist/postlist.component.ts
@@ -29,7 +29,58 @@ export class PostlistComponent implements OnInit {
 		public router: Router,
         public activeRoute: ActivatedRoute,
         public postService:PostlistService) {
+		
+		console.log("------这里开始对比Promise和Observable，这块代码是为了学习Observable使用的------");
 
+		//以下是Promise的写法
+		let promise = new Promise(resolve => {
+		    setTimeout(() => {
+		        resolve('---promise timeout---');
+		    }, 2000);
+		});
+		promise.then(value => console.log(value));
+
+		//以下是Observable的写法
+		let stream1$ = new Observable(observer => {
+		    let timeout = setTimeout(() => {
+		        observer.next('observable timeout');
+		    }, 2000);
+
+		    return () => {
+		        clearTimeout(timeout);
+		    }
+		});
+		let disposable = stream1$.subscribe(value => console.log(value));
+
+		//【第一个核心不同点来了】：Observable是可以中途取消的，而Promise一旦触发就不能取消了
+		// setTimeout(() => {
+		//     disposable.unsubscribe();
+		// }, 1000);
+
+		//【第二个核心不同点来了】：Observable可以持续发射很多值，而Promise只能发射一个值就结束了
+		let stream2$ = new Observable(observer => {
+		    let count = 0;
+		    let interval = setInterval(() => {
+		        observer.next(count++);
+		    }, 1000);
+
+		    return () => {
+		        clearInterval(interval);
+		    }
+		});
+		stream2$.subscribe(value => console.log(value));
+
+		//【第三个核心不同点来了】：Observable提供了很多的工具函数，最最最常用的filter和map演示如下
+		// stream2$
+		//   .filter(value => value % 2 == 0)
+		//   .subscribe(value => console.log(value));
+
+		// stream2$
+		//   .map(value => value * value)
+		//   .subscribe(value => console.log(value));
+
+
+		console.log("------------------------------------------------");
 	}
 
   	ngOnInit() {
