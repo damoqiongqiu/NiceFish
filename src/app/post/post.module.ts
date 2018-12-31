339a677ff8851820b71a38f19564dca5cdf32e9d
damoqiongqiu <damoqiongqiu@126.com>
2018-12-31 20:28:54 +0800

    更新PrimeNG的引入方式和分页工具样式

--- src/app/post/post.module.ts
+++ src/app/post/post.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
 
-import { PaginatorModule } from 'primeng/components/paginator/paginator';
-
+import { PaginatorModule } from 'primeng/paginator';
 import { PostDetailComponent } from './post-detail/post-detail.component';
 import { PostlistComponent } from './postlist/postlist.component';
 import { PostlistService } from './postlist/services/postlist.service';
