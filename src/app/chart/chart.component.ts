e5ea00bab633d9a773e79ebab737d9578f1842fc
hbxyxuxiaodong <1334339809@163.com>
2019-01-02 12:46:10 +0800

    add some style

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { fadeIn } from '../animations/fade-in';
-
+import Color from '../utils/color.util';
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
@@ -18,6 +18,7 @@ export class ChartComponent implements OnInit {
         }
       }
     ],
+    color: Color.baseColor,
     title: {
       text: 'NiceFish访问用户地区分布',
       subtext: '纯属虚构',
@@ -70,7 +71,7 @@ export class ChartComponent implements OnInit {
       subtext: '纯属虚构',
       x: 'center'
     },
-    color: ['#3398DB'],
+    color: Color.baseColor,
     tooltip: {
       trigger: 'axis',
       axisPointer: {            // 坐标轴指示器，坐标轴触发有效
@@ -103,6 +104,14 @@ export class ChartComponent implements OnInit {
         name: '访问量',
         type: 'bar',
         barWidth: '60%',
+        itemStyle: {
+          normal: {
+            color: params => {
+              const color = Color.genColor(this.barChart.series[0].data);
+              return color[params.dataIndex];
+            }
+          }
+        },
         data: [10, 52, 200, 334, 390, 330, 220, 1000, 500, 444, 999, 11]
       }
     ]
@@ -114,6 +123,7 @@ export class ChartComponent implements OnInit {
       subtext: '纯属虚构',
       x:"center"
     },
+    color: Color.baseColor,
     tooltip: {
       trigger: 'axis'
     },
@@ -132,9 +142,48 @@ export class ChartComponent implements OnInit {
       {
         name: '访问量',
         type: 'line',
-        data: [11, 11, 15, 13, 12, 13, 10,123,100,99,66,199]
+        data: [11, 11, 15, 13, 12, 13, 10, 123, 100, 19, 16, 199]
+      },
+      {
+        name: '访问量1',
+        type: 'line',
+        data: [21, 21, 25, 23, 22, 23, 20, 223, 200, 29, 26, 299]
+      },
+      {
+        name: '访问量2',
+        type: 'line',
+        data: [31, 31, 35, 33, 32, 33, 30, 323, 300, 39, 36, 399]
+      },
+      {
+        name: '访问量3',
+        type: 'line',
+        data: [41, 41, 45, 43, 42, 43, 40, 423, 400, 49, 46, 499]
+      },
+      {
+        name: '访问量4',
+        type: 'line',
+        data: [41, 41, 45, 43, 42, 43, 40, 423, 400, 49, 56, 499]
+      },
+      {
+        name: '访问量5',
+        type: 'line',
+        data: [51, 51, 55, 53, 52, 53, 50, 523, 500, 59, 66, 599]
+      },
+      {
+        name: '访问量6',
+        type: 'line',
+        data: [61, 61, 65, 63, 62, 63, 60, 623, 600, 69, 76, 699]
+      },
+      {
+        name: '访问量7',
+        type: 'line',
+        data: [71, 71, 75, 73, 72, 73, 70, 723, 700, 79, 86, 799]
+      },
+      {
+        name: '访问量8',
+        type: 'line',
+        data: [81, 81, 85, 83, 82, 83, 80, 823, 800, 89, 96, 899]
       }
-
     ]
   };
 
---
+++ src/app/utils/color.util.ts
@@ -0,0 +1,29 @@
+const baseColor = [
+  '#25859e',
+  '#6acece',
+  '#e78816',
+  '#eabc7f',
+  '#12619d',
+  '#ad2532',
+  '#15938d',
+  '#b3aa9b',
+  '#042d4c'
+];
+const genColor = (arr: any) => {
+  let num = 0;
+  let color = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (num < baseColor.length) {
+      color[i] = baseColor[num++];
+    } else {
+      num = 0;
+      color[i] = baseColor[num++];
+    }
+  }
+  return color;
+};
+
+export default {
+  baseColor,
+  genColor
+}
