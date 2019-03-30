d6651237ab2c3544a34e63c4a318481356079c95
damoqiongqiu <damoqiongqiu@126.com>
2019-03-30 21:50:12 +0800

    删掉不需要的代码:

--- src/main.server.ts
+++
@@ -1,8 +0,0 @@
-import { environment } from "./environments/environment";
-import { enableProdMode } from "@angular/core";
-
-if (environment.production) {
- enableProdMode();
-}
-
-export {AppServerModule} from "./app/app.server.module";
