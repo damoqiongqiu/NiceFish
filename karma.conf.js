ec95ff1383249dbe628eb400ea967d1a993de63f
大漠穷秋 <damoqiongqiu@126.com>
2023-03-21 22:16:33 +0800

    update code coverage config

--- karma.conf.js
+++ karma.conf.js
@@ -7,13 +7,30 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-coverage'),
+      require('karma-coverage-istanbul-reporter'),
       require('karma-htmlfile-reporter'),
       require('karma-mocha-reporter'),
-      require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/nice-fish'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ],
+      check: {
+        global: {
+          statements: 80,
+          branches: 80,
+          functions: 80,
+          lines: 80
+        }
+      }
     },
     coverageIstanbulReporter: {
       reports: [ 'html', 'lcovonly' ],
