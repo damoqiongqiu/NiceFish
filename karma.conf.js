37365886e1b0c6e733cbf583634cf94bf4431ea7
大漠穷秋 <damoqiongqiu@126.com>
2023-04-04 19:46:09 +0800

    update package.json and karma.config.js, ChromeHeadless is used by default.

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+const isWatch = require('yargs').strict(false).option('watch', { type: 'boolean', default: false }).argv.watch;
+const browser = isWatch ? 'Chrome' : 'ChromeHeadless';
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -52,7 +55,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: [browser],
     singleRun: true
   });
 };
