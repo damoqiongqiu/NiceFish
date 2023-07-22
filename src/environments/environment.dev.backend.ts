9df12caa1924ba8641e845e1d57553e312ae58e0
大漠穷秋 <damoqiongqiu@126.com>
2023-07-22 22:22:30 +0800

    删掉不用的环境变量标志

--- src/environments/environment.dev.backend.ts
+++ src/environments/environment.dev.backend.ts
@@ -3,7 +3,6 @@ import { dataURL } from './data-url-config/backend-url';
 
 //从服务端加载数据
 export const environment = {
-    envName:'dev.backqend',
     production: false,
     isMock:false,
     dataURL:dataURL
--- src/environments/environment.prod.backend.ts
+++ src/environments/environment.prod.backend.ts
@@ -3,7 +3,6 @@ import { dataURL } from './data-url-config/backend-url';
 
 //从服务端加载数据
 export const environment = {
-    envName:'prod.backqend',
     production: true,
     isMock:false,
     dataURL:dataURL
